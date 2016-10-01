210e8521c291e749f4f3ade8a93c571d0362f85e
Andres <frandres333@gmail.com>
2016-10-01 15:46:03 -0500

    Insert paperclip gem

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  resources :categories
+
+  devise_for :users
+  resources :articles do
+    resources :comments, only: [:create, :destroy, :update, :show]
+  end
+=begin
+  get "/articles"
+  post "/articles"
+  delete "/articles"
+  get "/articles/:id"
+  get "/articles/new"
+  get "/articles/:id/edit"
+  patch "/articles/:id"
+  put "/articles/:id"
+=end
+  root "welcome#index"
+end
